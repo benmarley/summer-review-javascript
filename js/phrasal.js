05a196e3cb9481eaa574bf8f4728940cb651df96
benmarley <benmarley@Bens-MacBook-Pro.local>
2015-09-05 15:02:12 -0400

    phrasal works

--- js/phrasal.js
+++ js/phrasal.js
@@ -1,11 +1,19 @@
 var $form = $('.form');
 var $story = $('.story')
+ 
 
-$form.on('submit', function (e){
+$('#words').on('submit', function (e){
 	e.preventDefault();
+
+	
+	
+	$('#words input').each(function (item){
+		var userAnswer = '#data-' + $(this).attr('data-answer');
+
+		$(userAnswer).html($(this).val());
+
+	});
 
 	$(this).addClass('hidden');
 	$story.addClass('show');
-	
-
 });
